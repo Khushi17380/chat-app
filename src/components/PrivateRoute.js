dd1d62c3fea89b8cb7cac9cd203cd05cf38cdcfd
Khushi <khushisahu850@gmail.com>
2021-09-19 00:14:03 +0530

    managed user profile context,punt time conxt

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { Redirect } from 'react-router';
 import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Container, Loader } from 'rsuite';
+import { useProfile } from '../context/profile.context';
 
 const PrivateRoute = ({children, ...routeProps}) => {
-  const profile=false;
-  if(!profile){
-    return <Redirect to="/SignIn" />
+  const {profile, isLoading}=useProfile();
+
+if(isLoading && !profile){
+  return (<Container>
+    <Loader center vertical size="md" content="Loading" speed="slow" />
+  </Container>
+  );}
+
+
+  if(!profile && !isLoading){
+    return <Redirect to="/signin" />
   }
   return (
     <Route {...routeProps}>{children}</Route>
   )
 }
 
-export default PrivateRoute
+export default PrivateRoute;
+
