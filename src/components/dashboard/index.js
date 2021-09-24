ac23b6cc7edcbcf024330d21083fcdc05ccfb656
Khushi <khushisahu850@gmail.com>
2021-09-24 13:44:27 +0530

    created last msg chat screen and user data with model window

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Drawer, Button, Divider, Alert } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
-import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
-import AvatarUploadBtn from './AvatarUploadBtn';
+import { database } from '../../misc/firebase';
 import ProviderBlock from './ProviderBlock';
+import AvatarUploadBtn from './AvatarUploadBtn';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
-  const onSave=async (newData)=>{
-    const userNicknameRef=database.ref(`/profiles/${profile.uid}`).child('name');
+  const onSave = async newData => {
     try {
-      await userNicknameRef.set(newData);
-      Alert.success('Nickname haas been updated',4000);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
+
+      Alert.success('Nickname has been updated', 4000);
     } catch (err) {
-      Alert.error(err.message,4000);
+      Alert.error(err.message, 4000);
     }
   };
 
@@ -29,11 +37,11 @@ const Dashboard = ({ onSignOut }) => {
         <h3>Hey, {profile.name}</h3>
         <ProviderBlock />
         <Divider />
-        <EditableInput 
-        name="nickname"
-        initialValue={profile.name}
-        onSave={onSave}
-        label={<h6 className="mb-2">Nickname</h6>}
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
         />
         <AvatarUploadBtn />
       </Drawer.Body>
