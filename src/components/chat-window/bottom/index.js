2d1c18c990e3eff1990bab00a8efb29503717823
Khushi <khushisahu850@gmail.com>
2021-09-26 00:18:43 +0530

    handled msg likes

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -6,87 +6,88 @@ import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
 
 function assembleMessage(profile, chatId) {
-  return {
-    roomId: chatId,
-    author: {
-      name: profile.name,
-      uid: profile.uid,
-      createdAt: profile.createdAt,
-      ...(profile.avatar ? { avatar: profile.avatar } : {}),
-    },
-    createdAt: firebase.database.ServerValue.TIMESTAMP,
-  };
+    return {
+        roomId: chatId,
+        author: {
+            name: profile.name,
+            uid: profile.uid,
+            createdAt: profile.createdAt,
+            ...(profile.avatar ? { avatar: profile.avatar } : {}),
+        },
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+        likeCount: 0,
+    };
 }
 
 const Bottom = () => {
-  const [input, setInput] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+    const [input, setInput] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-  const { chatId } = useParams();
-  const { profile } = useProfile();
+    const { chatId } = useParams();
+    const { profile } = useProfile();
 
-  const onInputChange = useCallback(value => {
-    setInput(value);
-  }, []);
+    const onInputChange = useCallback(value => {
+        setInput(value);
+    }, []);
 
-  const onSendClick = async () => {
-    if (input.trim() === '') {
-      return;
-    }
+    const onSendClick = async () => {
+        if (input.trim() === '') {
+            return;
+        }
 
-    const msgData = assembleMessage(profile, chatId);
-    msgData.text = input;
+        const msgData = assembleMessage(profile, chatId);
+        msgData.text = input;
 
-    const updates = {};
+        const updates = {};
 
-    const messageId = database.ref('messages').push().key;
+        const messageId = database.ref('messages').push().key;
 
-    updates[`/messages/${messageId}`] = msgData;
-    updates[`/rooms/${chatId}/lastMessage`] = {
-      ...msgData,
-      msgId: messageId,
+        updates[`/messages/${messageId}`] = msgData;
+        updates[`/rooms/${chatId}/lastMessage`] = {
+            ...msgData,
+            msgId: messageId,
+        };
+
+        setIsLoading(true);
+        try {
+            await database.ref().update(updates);
+
+            setInput('');
+            setIsLoading(false);
+        } catch (err) {
+            setIsLoading(false);
+            Alert.error(err.message);
+        }
     };
 
-    setIsLoading(true);
-    try {
-      await database.ref().update(updates);
+    const onKeyDown = ev => {
+        if (ev.keyCode === 13) {
+            ev.preventDefault();
+            onSendClick();
+        }
+    };
 
-      setInput('');
-      setIsLoading(false);
-    } catch (err) {
-      setIsLoading(false);
-      Alert.error(err.message);
-    }
-  };
+    return (
+        <div>
+            <InputGroup>
+                <Input
+                    placeholder="Write a new message here..."
+                    value={input}
+                    onChange={onInputChange}
+                    onKeyDown={onKeyDown}
+                />
 
-  const onKeyDown = ev => {
-    if (ev.keyCode === 13) {
-      ev.preventDefault();
-      onSendClick();
-    }
-  };
-
-  return (
-    <div>
-      <InputGroup>
-        <Input
-          placeholder="Write a new message here..."
-          value={input}
-          onChange={onInputChange}
-          onKeyDown={onKeyDown}
-        />
-
-        <InputGroup.Button
-          color="blue"
-          appearance="primary"
-          onClick={onSendClick}
-          disabled={isLoading}
-        >
-          <Icon icon="send" />
-        </InputGroup.Button>
-      </InputGroup>
-    </div>
-  );
+                <InputGroup.Button
+                    color="blue"
+                    appearance="primary"
+                    onClick={onSendClick}
+                    disabled={isLoading}
+                >
+                    <Icon icon="send" />
+                </InputGroup.Button>
+            </InputGroup>
+        </div>
+    );
 };
 
 export default Bottom;
