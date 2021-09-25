5e1273858d6483992e39065bb9d2991d39cb498c
Khushi <khushisahu850@gmail.com>
2021-09-25 22:18:17 +0530

    displayed real time presence

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -2,36 +2,39 @@ import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
 
 const MessageItem = ({ message }) => {
-  const { author, createdAt, text } = message;
+    const { author, createdAt, text } = message;
 
-  return (
-    <li className="padded mb-1">
-      <div className="d-flex align-items-center font-bolder mb-1">
-        <ProfileAvatar
-          src={author.avatar}
-          name={author.name}
-          className="ml-1"
-          size="xs"
-        />
+    return (
+        <li className="padded mb-1">
+            <div className="d-flex align-items-center font-bolder mb-1">
+                <PresenceDot uid={author.uid} />
 
-        <ProfileInfoBtnModal
-          profile={author}
-          appearance="link"
-          className="p-0 ml-1 text-black"
-        />
-        <TimeAgo
-          datetime={createdAt}
-          className="font-normal text-black-45 ml-2"
-        />
-      </div>
+                <ProfileAvatar
+                    src={author.avatar}
+                    name={author.name}
+                    className="ml-1"
+                    size="xs"
+                />
 
-      <div>
-        <span className="word-breal-all">{text}</span>
-      </div>
-    </li>
-  );
+                <ProfileInfoBtnModal
+                    profile={author}
+                    appearance="link"
+                    className="p-0 ml-1 text-black"
+                />
+                <TimeAgo
+                    datetime={createdAt}
+                    className="font-normal text-black-45 ml-2"
+                />
+            </div>
+
+            <div>
+                <span className="word-breal-all">{text}</span>
+            </div>
+        </li>
+    );
 };
 
 export default MessageItem;
