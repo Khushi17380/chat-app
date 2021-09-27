811269febfb516d83bc206eaf375819326ceb22b
Khushi <khushisahu850@gmail.com>
2021-09-27 13:47:13 +0530

    added cloud functions to send fcm msgs

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,4 @@
 const functions = require("firebase-functions");
-
-
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./service-account.json");
@@ -9,11 +7,6 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://chat-web-app-7b174-default-rtdb.firebaseio.com"});
 
+const {sendFcm}=require("./src/fcm");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendFcm =sendFcm;
